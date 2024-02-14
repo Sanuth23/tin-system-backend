7bf4c92a46c1338d6b4c2910f5dc11cc89771f53
Sanuth23 <sanuth0000@gmail.com>
2024-02-14 11:59:47 +0530

    create userdto & entity

---
+++ src/main/java/org/example/dto/User.java
@@ -0,0 +1,25 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+    private String nameWithInitials;
+    private String fullName;
+    private int NIC;
+    private String birthday;
+    private String birthCountry;
+    private String gender;
+    private String income;
+    private String address;
+    private String contactNumber;
+    private String civilStatus;
+    private String profession;
+
+}
---
+++ src/main/java/org/example/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String nameWithInitials;
+    private String fullName;
+    private int NIC;
+    private String birthday;
+    private String birthCountry;
+    private String gender;
+    private String income;
+    private String address;
+    private String contactNumber;
+    private String civilStatus;
+    private String profession;
+
+}
